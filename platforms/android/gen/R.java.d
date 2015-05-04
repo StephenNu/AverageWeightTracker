1111b97a67e8e34770c596bd34478786cb246455
Stephen Nusko <stephen.tkn@gmail.com>
2015-05-04 14:42:11 -0400

    Currently just saving for a dangarous system change, will go through and edit it correctly later

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+C:\donnam-docs\PhoneGap-quickstart\ZUMOAPPNAME_phonegap\ZUMOAPPNAME\platforms\android\gen\com\mobileservices\ZUMOAPPNAME\R.java \
+ : C:\donnam-docs\PhoneGap-quickstart\ZUMOAPPNAME_phonegap\ZUMOAPPNAME\platforms\android\res\drawable\icon.png \
+C:\donnam-docs\PhoneGap-quickstart\ZUMOAPPNAME_phonegap\ZUMOAPPNAME\platforms\android\res\drawable-hdpi\icon.png \
+C:\donnam-docs\PhoneGap-quickstart\ZUMOAPPNAME_phonegap\ZUMOAPPNAME\platforms\android\res\drawable-ldpi\icon.png \
+C:\donnam-docs\PhoneGap-quickstart\ZUMOAPPNAME_phonegap\ZUMOAPPNAME\platforms\android\res\drawable-mdpi\icon.png \
+C:\donnam-docs\PhoneGap-quickstart\ZUMOAPPNAME_phonegap\ZUMOAPPNAME\platforms\android\res\drawable-xhdpi\icon.png \
+C:\donnam-docs\PhoneGap-quickstart\ZUMOAPPNAME_phonegap\ZUMOAPPNAME\platforms\android\res\values\strings.xml \
+C:\donnam-docs\PhoneGap-quickstart\ZUMOAPPNAME_phonegap\ZUMOAPPNAME\platforms\android\res\xml\config.xml \
+C:\donnam-docs\PhoneGap-quickstart\ZUMOAPPNAME_phonegap\ZUMOAPPNAME\platforms\android\bin\AndroidManifest.xml \
